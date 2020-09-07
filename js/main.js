1dd5e8feeae4df99cf60b553ce3a2b555bdcf948
Axel Barragan <axelmontes92@gmail.com>
2020-09-06 19:50:17 -0500

    Avances generales
    Avances generales

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 /*JS*/
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+  	 nav:false,
+  	 items: 1,
+  	 loop: true,
+  	 height: 700,
+  	 autoplay: true,
+  	 autoplayTimeout:4500,
+  });
+});
